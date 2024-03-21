b587652aefa9a0bd7d925ae128aa33e5224d7edb
garrett_powers <garrett.powers@agnet.tamu.edu>
2024-03-21 09:57:54 -0500

    Use arrays

--- script.js
+++ script.js
@@ -3,82 +3,126 @@
 //     this.output = output;
 // };
 const makeKey = (val) => {
-    const key = document.createElement('button');
-    key.textContent = val;
-    key.setAttribute('class', 'key');
-    return key;
-    key.addEventListener('click', () => keyClick);
+  const key = document.createElement("button");
+  key.textContent = val;
+  key.setAttribute("class", "key");
+  return key;
 };
 
 const makeCalculator = (parent) => {
-    const calcDiv = document.createElement('div');
-    calcDiv.setAttribute('class', 'calculator');
+  const calcDiv = document.createElement("div");
+  calcDiv.setAttribute("class", "calculator");
 
-    const display = document.createElement('div');
-    display.setAttribute('class', 'display');
-    display.textContent = displayVal;
+  const display = document.createElement("div");
+  display.setAttribute("class", "display");
 
-    const keypad = document.createElement('div');
-    keypad.setAttribute('class', 'keypad');
+  const keypad = document.createElement("div");
+  keypad.setAttribute("class", "keypad");
 
-    parent.appendChild(calcDiv);
-    calcDiv.appendChild(display);
-    calcDiv.appendChild(keypad);
-    
-    const keys = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "C", "&divide", "&times", "+", "-", "="
-    ];
-    keys.map((k) => {
-        const newkey = makeKey(k);
-        keypad.appendChild(newkey);
+  parent.appendChild(calcDiv);
+  calcDiv.appendChild(display);
+  calcDiv.appendChild(keypad);
+
+  const numKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const opKeys = ["C", "/", "*", "+", "-",];
+  numKeys.map((k) => {
+    const newkey = makeKey(k);
+    keypad.appendChild(newkey);
+    newkey.addEventListener("click", () => {
+        updateVals(newkey.textContent);
+        updateDisplay(values);
+        console.log(values);
     });
+  });
+  opKeys.map((k) => {
+    const newkey = makeKey(k);
+    keypad.appendChild(newkey);
+      newkey.addEventListener("click", () => {
+        updateEquation(newkey.textContent);
+        console.log(equation);
+      });
+  });
+  const equalsKey = makeKey("=");
+  keypad.appendChild(equalsKey);
+  equalsKey.addEventListener("click", () => evaluate())
 };
 
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
 
-const divide = (a, b) => a/b;
+const divide = (a, b) => a / b;
 
-const multiply = (a, b) => a*b;
+const multiply = (a, b) => a * b;
 
 const operate = (op, a, b) => {
-    var output = 0;
-    switch(op) {
-        case "/":
-            output = divide(a,b);
-            break
-        case "*":
-            output = multiply(a,b);
-            break;
-        case "+":
-            output = add(a,b);
-            break;
-        case "-":
-            output = subtract(a,b);
-            break;
-    }
-    updateDisplay(output);
+  var output = 0;
+  switch (op) {
+    case "/":
+      output = divide(a, b);
+      break;
+    case "*":
+      output = multiply(a, b);
+      break;
+    case "+":
+      output = add(a, b);
+      break;
+    case "-":
+      output = subtract(a, b);
+      break;
+  }
+  updateDisplay(output);
 };
 
-const updateDisplay = (input) => {
-    const display = document.querySelector(".display")
-    if (input = "c") {
-        clearValues();
-        display.textContent = "";
-    }
-};
-
-const clearValues = () => {
-    a = 0;
-    b = 0;
-    operator = "";
+const evaluate = (eq) => {
+    
 }
 
-let a = 0;
-let b = 0;
-let operator = "";
-let displayVal = "80085";
+const updateDisplay = (vals) => {
+  const display = document.querySelector(".display");
+  
+    displayVal = vals.join("");
+    display.textContent = displayVal;
+};
+
+const updateVals = (input) => {
+    values.push(input);
+}
+
+const clearValues = () => {
+  a = null;
+  b = null;
+  operator = null;
+};
+
+const loadValues = (input) => {
+    if (!valA) {
+        valA = input;
+    }
+    else if (valA && operator) {
+        valB = input;
+    }
+}
+
+const updateEquation = (input) => {
+    console.log(input);
+    // If the last part of an equation is an operator, replace it
+    if (typeof +equation[equation.length -1] !== 'number'){
+        console.log("not a number!")
+        equation[equation.length - 1] = input;
+    }
+    else {
+        equation.push(+values.join(""));
+        values = [];
+        equation.push(input);
+    }
+    
+}
+
+let values = [];
+let equation = [];
+let operator = null;
+let displayVal = "";
 
 const container = document.querySelector(".container");
 makeCalculator(container);
